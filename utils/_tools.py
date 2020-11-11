a5341c123e589f57c00de4e17430df288657a0a0
Scc_hy <34824890+scchy@users.noreply.github.com>
2020-11-11 21:03:07 +0800

    Create _tools.py

---
+++ utils/_tools.py
@@ -0,0 +1,25 @@
+# python 3.6
+# Author: Scc_hy
+# Create date: 2020-08-20
+# Function: mnist数据加载，和装饰器
+
+import pandas as pd 
+import numpy as np
+from sklearn.datasets import fetch_mldata
+import warnings
+warnings.filterwarnings(action='ignore')
+import time
+from functools import wraps
+
+
+def clock(func):
+    @wraps(func)
+    def clocked(*args, **kwargs):
+        st = time.perf_counter()
+        res = func(*args, **kwargs)
+        take_time = time.perf_counter() - st
+        fmt = '{func_name}, take_time:{take_time:.5f}s >> {res}'
+        print(fmt.format(func_name=func.__name__, take_time=take_time, res=res))
+        return res
+    return clocked
+
